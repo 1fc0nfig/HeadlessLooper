3db9b6d1690b9906433484a2cd4392316a4884cb
Matyas Cerny <1fconfig@protonmail.com>
2023-12-29 17:53:40 +0100

    installation script and service added

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+# Installation
+```
+sudo sh https://raw.githubusercontent.com/1fc0nfig/HeadlessLooper/main/install.sh
+```
--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y git vlc
+
+# Check if there is a directory called "HeadlessLooper" in currect directory
+# If not, clone the repo
+if [ ! -d "HeadlessLooper" ]; then
+    git clone https://github.com/1fc0nfig/HeadlessLooper.git
+fi
+
+# Change directory to HeadlessLooper
+cd HeadlessLooper
+
+sudo mkdir /boot/Videos
+sudo cp headlesslooper.service /etc/systemd/system/headlesslooper.service
+sudo systemctl enable headlesslooper.service
